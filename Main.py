15593a3f1d6f0a49f6625183da34717ac6746369
Bill Dickens <wtdickens@gmail.com>
2019-05-23 12:48:05 -0400

    Second attempt at commit in VS code

---
+++ Main.py
@@ -0,0 +1,6 @@
+"""
+    Main program for Dickens, Triest and Sederberg's work on the impact of income shocks on well being
+    This is a change to test commit
+    Here is yet another change to this header file and a little bit of python code below
+"""
+print("This is the main program")
