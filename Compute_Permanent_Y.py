2d0132a80b28f2ded504dbe71fc2f55804dca857
Bill Dickens <wtdickens@gmail.com>
2019-07-02 19:07:09 -0400

    Adding program to compute permanent income matrix awith updates to main

---
+++ Compute_Permanent_Y.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+def Compute_Permanent_Y(age,ed,experience,unemployment,sex,Y)
+    """
+    Takes age, years of education, years of job experience, months of 
+    unemployment, sex and Fulltime income to compute a matrix of possible
+    values for permanent income in each remaining year in person's
+    working life.
+
+    Parameters
+    ----------
+    age : integer
+        person's age
+    ed : integer
+        years of education completed
+    experience : integer
+        full years of job experience
+    unemployment : integer 
+        months of unemployment in previous 12 months
+    sex : character
+        m for male, f for female
+    Y : integer
+        Fulltime equivalent annual income
+
+    Returns
+    -------
+    PYMat : numpy matrix
+        Each colulmn of the matrix contains the possible values for permanent 
+        income in each remaining year of the person's working life
+
+    Created on Tue Jul  2 18:43:22 2019
+
+    @author: wtdickens
+    """
+    
+    # For now this progrm creates a 2x(67-age) matrix with $60,000 in the upper
+    # row and 0 in the lower row
+    
+    PYMat=np.mat(np.zeros((2,67-age)))
+    for i in range(67-age):
+        PYMat[0,i]=60000
+        PYMat[1,i]=0
+    
+    return(PYMat)
+        
+
--- Main.py
+++ Main.py
@@ -14,37 +14,63 @@ Years_of_Retirement= 20
 N=2    # Number of values taken by permanent income each period
 K=5    # Number of possible values for wealth in each period
 Retirement_Age=67 # Assumed retirement age for all individuals
+RealRate=.02      # Real rate of return on savings
 
 # Classes
 
 class People:
-    # This class' initialization takes a persons crra, age, education, sex, 
-    # work experience, and recent employment history, to computes the 
+    # This class' initialization takes a person's crra, age, education, sex, 
+    # work experience, most recent full time annual income and recent 
+    # employment history, to computes the 
     # possible values for permanent income and wealth in each remaining
     # period of life.
     
     def __init__(self,crra=.9,education=12,sex="m",age=18,
-                 experience=-9,MonthsUnemployed=0,StartingWealth=-9):
+                 experience=-9,MonthsUnemployed=0,StartingWealth=-9
+                 ,FulltimeY=-9):
         
         # Create attributes for each incidence of the class
         #   crra is coefficient of relative risk aversion, 
         #   education is years completed
         #   experience is years of work experience
+        #   FulltimeY is the most recent full time annual income
         #   others are self explanitory
         self.crra=crra
+        self.education
         self.age=age
+        self.MonthsUnemployed=MonthsUnemployed
+        
         if experience < 0:
             self.experience=age-education
         else:
-            self.experince=experience
-        self.MonthsUnemployed=MonthsUnemployed
+            self.experience=experience
+        
+        if FulltimeY<0:
+            # If full time incoe is missing assume that it is $60k times
+            #  an adjustment for education assuming a rate of return of 
+            #  .07 per year completed
+            self.FulltimeY=60000*np.exp(.07*(education-12))
+        else:
+            self.FulltimeY=FulltimeY
+            
         if StartingWealth < 0:
-            StartingWealth=(age-18)*
+            # If no starting wealth assume constant Y and savings rate
+            #  of .16 which is optimal with no risk and RealRate of 2
+            #  if people work for 50 years, are retired for 20 and can
+            #  purchase a fair anuity on retirement
+            self.StartingWealth=.16*FulltimeY*.(np.exp(RealRate*(age-18))-1)
+                                /RealRate
+        else:
+            self.StartingWealth=StartingWealth
         
         # Now call function to create matrix of possible permanent income 
         #   values in each period as an Nx(Retirement_Age-Age) matrix
-        self.PermYMat=Compute_Permanent_Y(self.age,self.crra,self.experience,
-                                          self.MonthsUnemployed)
+        self.PermYMat=Compute_Permanent_Y(self.age,
+                                          self.education,
+                                          self.experience,
+                                          self.MonthsUnemployed,
+                                          self.sex,
+                                          self.FulltimeY)
         
         # Finally call function to compute matrix of possible values of 
         #  wealth in each remaining period of life
